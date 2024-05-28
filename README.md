f719ef5b5c6ce69e47c730dc211c804306cf0af8
ankibar <ankit.220610@gmail.com>
2024-05-29 02:04:09 +0530

    done with read update and delete and final thoughts in readme

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # learnfastapi
 Just a demo project to learn fastapi to perform basic CRUD
 
-Checking if git works locally
+Thoughts:-
+1. The lack of syntactic sugar was welcome but it does take time to know where we use classes and objects
+2. Installed DB Browser for SQLLite on my Windows machine. Not bad if you want to see how the .db file looks
+3. CRUD was pretty straightforward but not a fan of orm. It adds more complexity than one would like
+4. Being a SQL guy, need to find a way to make use of those skills instead of remembering the ToDo class is a table and todo is an object
+5. This mini effort was courtesy of the tutorial at [here](https://www.gormanalysis.com/blog/building-a-simple-crud-application-with-fastapi)
+6. Didn't refactor the code further because the lack of wielding SQL for doing something simple was getting incovenient
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,status
+from fastapi import FastAPI,status, HTTPException
 from database import Base, engine, ToDo
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
@@ -14,7 +14,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return "todooo"
+    return "welcome to my app"
 
 @app.post("/todo", status_code = status.HTTP_201_CREATED)
 def create_todo(todo: ToDoRequest):
@@ -28,16 +28,37 @@ def create_todo(todo: ToDoRequest):
 
 @app.get("/todo/{id}")
 def read_todo(id: int):
-    return "read todo item with id {id}"
+    session =  Session(bind=engine, expire_on_commit=False)
+    todo = session.query(ToDo).get(id)
+    session.close()
+    return f"todo item with id: {todo.id} and task: {todo.task}"
 
 @app.put("/todo/{id}")
-def update_todo(id: int):
+def update_todo(id: int, task: str):
+    session = Session(bind=engine, expire_on_commit=False)
+    todo = session.query(ToDo).get(id)
+    if todo:
+        todo.task = task
+        session.commit()
+    session.close()
+    if not todo:
+        raise HTTPException(status_code=404, detail=f"todo item with id {id} not found")
     return "update todo item with id {id}"
 
 @app.delete("/todo/{id}")
 def delete_todo(id: int):
-    return "delete todo item with id {id}"
+    session = Session(bind=engine, expire_on_commit=False)
+    todo = session.query(ToDo).get(id)
+    if todo:
+        session.delete(todo)
+        session.commit()
+        session.close()
+    else:
+        raise HTTPException(status=404, details = "deleted")
+    return None
 
 @app.get("/todo")
 def read_todo_list():
-    return "read todo list"
+    session = Session(bind=engine, expire_on_commit=False)
+    todo = session.query(ToDo).all()
+    return todo
